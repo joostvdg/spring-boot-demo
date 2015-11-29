1bebe405905346e3c6a8e9afee2eeafc35e95476
Joost van der Griendt <joostvdg@gmail.com>
2015-11-29 11:58:36 +0100

    Initial commit

---
+++ src/main/java/nl/cam/demo/spring/Example.java
@@ -0,0 +1,28 @@
+package nl.cam.demo.spring;
+
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+
+}
